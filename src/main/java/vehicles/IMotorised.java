152eb82ddf68d36b759100594c738f92ae53abcb
Janet_Mensah <janetmensah60@gmail.com>
2023-10-18 22:42:36 +0100

    Fixed methods for SpeedBoat

--- src/main/java/vehicles/IMotorised.java
+++ src/main/java/vehicles/IMotorised.java
@@ -1,7 +1,7 @@
 package vehicles;
 
 public interface IMotorised {
-    void startEngine();
+    void startEngine(); //we don't want these to be hard coded we want this interface class to be used for others
     void stopEngine();
     int getHp();
     void setHp(int hp);
--- src/main/java/vehicles/Speedboat.java
+++ src/main/java/vehicles/Speedboat.java
@@ -1,41 +1,51 @@
 package vehicles;
 
 import products.IProduct;
-
-public class Speedboat extends Vehicle implements IMotorised, IWaterVehicle{
+public class Speedboat extends Vehicle implements IMotorised, IWaterVehicle {
     private String hullType;
     private IMotorised motor;
-    public Speedboat(float weight, int maxSpeed, IProduct baseProduct, String hullType, IMotorised motor){
-        super(weight,maxSpeed,baseProduct);
+
+    public Speedboat(float weight, int maxSpeed, IProduct baseProduct, String hullType, IMotorised motor) {
+        super(weight, maxSpeed, baseProduct);
         this.hullType = hullType;
         this.motor = motor;
     }
 
-    public void startEngine(){
+    public void startEngine() {
+        System.out.println("SpeedBoat Engine started");
 
     }
-    public void stopEngine(){
-        
-    }
-    public int getHp(){
+
+    public void stopEngine() {
+        System.out.println("SpeedBoat Engine Stopped");
 
     }
-    public void setHp(int hp){
+
+    public int getHp() {
+        return this.motor.getHp();
 
     }
-    public int getFuel(){
+
+    public void setHp(int hp) {
+        this.motor.setHp(hp);
 
     }
-    public void setFuel(int fuel){
-        this.fuel
+
+    public int getFuel() {
+        return this.motor.getFuel();
     }
 
-    String getHullType(){
-        return this.hullType;
-    }
-    String setHullType(String type){
-        this.hullType = type;
+    public void setFuel(int fuel) {
+    };
+
+     public String getHullType() {
+        return this.hullType; // using this.hulltype directly accesses the instance variable
+        // otherwise doing this.motor.hullType would imply that "hulltype is a field within motor
     }
 
+    public String setHullType(String type) {
+        return this.hullType = type;
+    }
 
 }
+
