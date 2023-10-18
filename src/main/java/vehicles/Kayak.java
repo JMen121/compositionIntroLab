b896faf126d8c88d6ad3f515d88dc282a93bdb3c
Janet_Mensah <janetmensah60@gmail.com>
2023-10-18 22:57:21 +0100

    Completed Kayak class, has been composed with the IWaterVehicle interface

--- src/main/java/vehicles/Kayak.java
+++ src/main/java/vehicles/Kayak.java
@@ -1,5 +1,18 @@
 package vehicles;
 
-public class Kayak {
-    
+public class Kayak implements IWaterVehicle {
+
+    private String hullType;
+
+    public Kayak(String hullType){
+        this.hullType = hullType;
+    }
+
+    public String getHullType(){
+        return this.hullType;
+    }
+
+    public String setHullType(String type){
+        return this.hullType = type;
+    }
 }
